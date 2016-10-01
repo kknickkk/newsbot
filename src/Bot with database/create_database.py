b75e35a8b7844ac6eb7096f41a5de8a23f160465
nickdurante <ng.durante@gmail.com>
2016-10-01 12:00:40 +0200

    Bugfixes

--- src/Bot with database/create_database.py
+++ src/Bot with database/create_database.py
@@ -10,7 +10,8 @@ users = []
 def main():
    
     update_id = 0
-    
+    read_database()
+	
     try:
         update_id = bot.getUpdates()[0].update_id
     except IndexError:
@@ -26,58 +27,66 @@ def main():
                     print("Error: user unauthorized")
                     update_id += 1
 
-        	
-			
+
+
 def create_database(bot, update_id):
 		
-		log = open('log.txt', 'a')
+	log = open('log.txt', 'a')
+	for update in bot.getUpdates(offset=update_id, timeout=10):
+		chat_id = update.message.chat_id
+       		update_id = update.update_id + 1
+        	message = update.message.text
+		firstname = update.message.from_user.first_name
+        	lastname = update.message.from_user.last_name
+        	iduser = str(update.message.from_user.id)
+		info = str(chat_id) + ' ' + firstname + ' ' + lastname 
 		
-			for update in bot.getUpdates(offset=update_id, timeout=10):
-				chat_id = update.message.chat_id
-        update_id = update.update_id + 1
-        message = update.message.text
-				firstname = update.message.from_user.first_name
-        lastname = update.message.from_user.last_name
-        iduser = str(update.message.from_user.id)
-				info = chat_id + firstname + lastname 
-				if message == '/start':
-					welcome_message(firstname)
-					log.write( info + ' started the bot\n')
+		if message == '/start':
+			welcome_message(chat_id, firstname)
+			log.write( info + ' started the bot\n')
 				
-				if message == '/subscribe':
-					if chat_id not in users:
-						users.append(chat_id)
-						database = open('database.db', 'a')
-						database.write(chat_id + '\n')
-						database.close()
-						log.write(info + ' subscribed\n')
-					else:
-						bot.sendMessage(chat_id, 'Already subscribed!\n')
-						log.write(info + 'has already subscribed\n')
+		if message == '/subscribe':
+			if chat_id not in users:
+				users.append(chat_id)
+				database = open('database.db', 'a')
+				database.write(str(chat_id) + '\n')
+				database.close()
+				log.write(info + ' subscribed\n')
+				bot.sendMessage(chat_id, 'Successfully subscribed\n')
+			else:
+				bot.sendMessage(chat_id, 'Already subscribed!\n')
+				log.write(info + 'has already subscribed\n')
 				
-				if message == '/unsubscribe':
-					try:
-						users.remove(chat_id)
-						database = open('database.db', 'w')
-						log.write(info + ' unsubscribed\n')
-						for item in users:
-							database.write("%d\n" % item)
-							database.close()
+		if message == '/unsubscribe':
+			try:
+				users.remove(chat_id)
+				database = open('database.db', 'w')
+				log.write(info + ' unsubscribed\n')
+				bot.sendMessage(chat_id, 'Successfully unsubscribed\n')
+				for item in users:
+					database.write("%d\n" % item)
+					database.close()
 
-					except:
-						bot.sendMessage(chat_id, 'Not subscribed!\n')
-						log.write(info + ' has already unsubscribed\n')
+			except:
+				bot.sendMessage(chat_id, 'Not subscribed!\n')
+				log.write(info + ' has already unsubscribed\n')
 			
-			log.close()			
-			return update_id
+	log.close()			
+	return update_id
 			
 
-def welcome_message(name):
+def welcome_message(chat_id, name):
 	welcome = 'Welcome ' + name +'\nPrompt /subscribe to receive news daily\n'
 	bot.sendMessage(chat_id=chat_id, text = welcome )
 
-			
-			
+def read_database():
+	try:
+		with open('database.db', 'r') as database:
+			for chat_id in database:
+				users.append(chat_id)
+	except:
+		print('Empty database\n')
+		
 if __name__ == '__main__':
     main()
     
